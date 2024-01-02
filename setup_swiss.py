2eefa033663867a15106131ae584eb5c58bcdd36
alorsunique <sounique7800@gmail.com>
2024-01-02 22:30:10 +0800

    2024 January 2 Created setup file

---
+++ setup_swiss.py
@@ -0,0 +1,44 @@
+# Setup file for Swiss
+
+import os
+from pathlib import Path
+
+# Create the resource path text file
+
+resources_dir_text = "Resources_Path.txt"
+
+with open(resources_dir_text, 'a') as writer:
+    writer.close()
+
+# Read the directory
+
+entry_list = []
+
+with open(resources_dir_text, 'r') as reader:
+    entry_list.append(reader.read())
+    reader.close()
+
+# Create the necessary folders
+
+if entry_list[0]:
+    resources_dir = Path(entry_list[0])
+    print(f"Resources Directory: {resources_dir}")
+
+    if not resources_dir.exists():
+        os.mkdir(resources_dir)
+
+    input_dir = resources_dir / "Input"
+    if not input_dir.exists():
+        os.mkdir(input_dir)
+
+    move_dir = resources_dir / "Move"
+    if not move_dir.exists():
+        os.mkdir(move_dir)
+
+    rescale_dir = resources_dir / "Rescale"
+    if not rescale_dir.exists():
+        os.mkdir(rescale_dir)
+
+    cropped_dir = resources_dir / "Cropped"
+    if not cropped_dir.exists():
+        os.mkdir(cropped_dir)
