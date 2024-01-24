bd4bab514d8876b4c3de0d82fc953634409dc35f
alorsunique <sounique7800@gmail.com>
2024-01-24 23:58:17 +0800

    2024 January 24

--- Album Art Recover/image_stitch.py
+++ Album Art Recover/image_stitch.py
@@ -16,6 +16,18 @@ temporary_workspace_dir = resources_dir / "Temporary Workspace"
 
 # Perform preliminary rename here
 
+file_list = []
+
+for image_file in input_dir.iterdir():
+    file_list.append(image_file)
+
+image_amount = len(file_list)
+count = 0
+for entry in file_list:
+    new_name = f"{str(image_amount-count).zfill(4)}.jpg"
+    count += 1
+    new_path = input_dir / new_name
+    os.rename(entry,new_path)
 
 file_list = []
 width_list = []
@@ -34,7 +46,9 @@ print(f"Height: {height_list}")
 max_width = max(width_list)
 max_height = max(height_list)
 
-joined_image = Image.new("RGB", (max_width * 3, height))
+print(f"Max Width: {max_width} | Max Height: {max_height}")
+
+joined_image = Image.new("RGB", (max_width * 3, max_height))
 
 count = 0
 vertical_count = 0
@@ -42,13 +56,19 @@ total_width = 0
 while count < len(file_list):
     current_image = Image.open(file_list[count])
     width = width_list[count]
-    total_width += width
-    joined_image.paste(current_image, (total_width - width, 0))
+    height = height_list[count]
+
+    if width < max_width or height < max_height:
+        print(f"Resizing")
+        current_image = current_image.resize((max_width, max_height), Image.LANCZOS)
+
+    total_width += max_width
+    joined_image.paste(current_image, (total_width - max_width, 0))
     count += 1
     if count % 3 == 0:
         total_width = 0
         vertical_count += 1
-        output_path = temporary_workspace_dir / f"Horizontal_{vertical_count}.jpg"
+        output_path = temporary_workspace_dir / f"Horizontal_{str(vertical_count).zfill(4)}.jpg"
         joined_image.save(output_path)
 
 horizontal_file_list = []
@@ -74,9 +94,15 @@ count = 0
 total_height = 0
 while count < len(horizontal_file_list):
     current_image = Image.open(horizontal_file_list[count])
+    width = horizontal_width_list[count]
     height = horizontal_height_list[count]
-    total_height += height
-    joined_image.paste(current_image, (0, total_height - height))
+
+    if width < max_width or height < max_height:
+        print(f"Resizing")
+        current_image = current_image.resize((max_width, max_height), Image.LANCZOS)
+
+    total_height += max_height
+    joined_image.paste(current_image, (0, total_height - max_height))
     count += 1
 
 final_width, final_height = joined_image.size
@@ -87,4 +113,7 @@ output_path = stitch_dir / f"Stitch_{current_time}.jpg"
 joined_image.save(output_path)
 
 for file in horizontal_file_list:
+    os.remove(file)
+
+for file in file_list:
     os.remove(file)
