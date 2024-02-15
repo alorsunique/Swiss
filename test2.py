39e048f7799854dcbda4e92ea5e325200beddb81
alorsunique <sounique7800@gmail.com>
2024-02-15 15:42:04 +0800

    2024 February 15

--- test2.py
+++ test2.py
@@ -4,14 +4,20 @@ from pathlib import Path
 
 script_path = Path(__file__)
 
-project_dir = script_path.parent.parent
+project_dir = script_path.parent
 os.chdir(project_dir)
 
 # sys.path.insert(0, str(project_dir/"Modules"))
 
-sys.path.append(str(project_dir / "Modules"))
+module_dir = project_dir / "Modules"
+os.chdir(module_dir)
+
+print(Path.cwd())
 
 import modtime
+
+os.chdir(project_dir)
+
 
 with open("Resources_Path.txt", "r") as resources_text:
     resources_dir = Path(resources_text.readline())
