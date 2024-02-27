872dd830fb95f2b4995e1cac01c39a759e73e989
alorsunique <sounique7800@gmail.com>
2024-02-27 21:18:57 +0800

    2024 February 27

--- Video/frame_extract.py
+++ Video/frame_extract.py
@@ -1,12 +1,10 @@
 # This script should extract a subclip from a video file
 
-import gc
 import os
 import sys
 from pathlib import Path
 
 from moviepy.editor import VideoFileClip
-from PIL import  Image
 
 script_path = Path(__file__).resolve()
 project_dir = script_path.parent.parent
@@ -22,78 +20,63 @@ workspace_dir = resources_dir / "Temporary Workspace"
 # Changes the working directory to the workspace
 os.chdir(workspace_dir)
 
-clip_input_dir = video_folder_dir / "Clip Input"
-if not clip_input_dir.exists():
-    os.mkdir(clip_input_dir)
+input_dir = video_folder_dir / "Input"
+output_dir = video_folder_dir / "Output"
 
-clip_output_dir = video_folder_dir / "Clip Output"
-if not clip_output_dir.exists():
-    os.mkdir(clip_output_dir)
+file_list = []
 
-
-input_list = []
-
-for entry in clip_input_dir.rglob('*'):
+for entry in input_dir.rglob('*'):
     if entry.is_file():
-        input_list.append(entry)
+        file_list.append(entry)
 
 break_condition = False
 
 # Main loop of the program
 while True:
-    in_count = 0
+    count = 0
     print(f"0: Exit Loop")
-    for input_video in input_list:
-        print(f"{in_count + 1}: {input_list[in_count].name}")
-        in_count += 1
+    for input_video in file_list:
+        print(f"{count + 1}: {file_list[count].name}")
+        count += 1
 
     try:
         choice = int(input(f"Select Option: "))
 
         if int(choice) == 0:
             break_condition = True
-        elif int(choice) < 0 or int(choice) > len(input_list):
+        elif int(choice) < 0 or int(choice) > len(file_list):
             print(f"Out of Bound")
         else:
-            # Creation of clips is done here
+            selected_file = file_list[choice - 1]
 
-            entry = input_list[choice - 1]
-
-            output_folder_dir = clip_output_dir / f"{entry.name}"
+            output_folder_dir = output_dir / f"{selected_file.stem}"
             if not output_folder_dir.exists():
                 os.mkdir(output_folder_dir)
 
-            print(f"Video: {entry.stem}")
+            print(f"Video: {selected_file.stem}")
             t_i = int(input(f"Starting Time: "))
             t_f = int(input(f"Ending Time: "))
 
-            video = VideoFileClip(str(entry))
+            video = VideoFileClip(str(selected_file))
             clip = video.subclip(t_i, t_f)
 
             fps = clip.fps
             frames = clip.iter_frames(fps=fps)
             duration = clip.duration
-            frame_estimate = int(duration * fps)
 
-            new_frame_list = []
-            count = 0
-
-            frame_time = 1/fps
+            frame_count = 0
+            frame_time = 1 / fps
 
             for frame in frames:
-                count += 1
-                print(f"Count: {count:0{8}}")
-                im = Image.fromarray(frame)
-                output_name = f"{entry.name}_{t_i:0{8}}-{t_f:0{8}}_Frame_{count:0{8}}.jpg"
+                frame_count += 1
+                print(f"Count: {frame_count:0{6}}")
+
+                output_name = f"{selected_file.name}_{str(t_i).zfill(5)}-{str(t_f).zfill(5)}_Frame_{str(frame_count).zfill(6)}.jpg"
                 output_path = output_folder_dir / output_name
-
-                clip.save_frame(output_path,t=frame_time*count)
-
-                #im.save(output_path, quality=100)
+                clip.save_frame(output_path, t=frame_time * frame_count)
 
             clip.close()
             video.close()
-
     except:
         print(f"Did not catch that")
 
@@ -101,6 +84,6 @@ while True:
         break
 
 # Clearing of the temporary workspace
-for entry in workspace_dir.iterdir():
-    print(f"Removing: {entry.name}")
-    os.remove(entry)
+for file in workspace_dir.iterdir():
+    print(f"Removing: {file.name}")
+    os.remove(file)
