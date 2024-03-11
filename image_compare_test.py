412f075f03041e0e12752daa9f5bd7f95f310c12
alorsunique <sounique7800@gmail.com>
2024-03-11 16:09:33 +0800

    2024 March 11

--- image_compare_test.py
+++ image_compare_test.py
@@ -1,9 +1,11 @@
+import itertools
 import os
 from pathlib import Path
+
 import cv2
-import itertools
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def feature_matching(image_path_1, image_path_2):
     image_1 = cv2.imread(str(image_path_1), cv2.IMREAD_GRAYSCALE)
@@ -14,43 +16,45 @@ def feature_matching(image_path_1, image_path_2):
     key_points_1, descriptor_1 = sift.detectAndCompute(image_1, None)
     key_points_2, descriptor_2 = sift.detectAndCompute(image_2, None)
 
+    print(f"{len(key_points_1)} | {len(key_points_2)}")
+
     FLANN_INDEX_KDTREE = 1
     index_parameters = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_parameters = dict(checks=50)
 
-    FLANN = cv2.FlannBasedMatcher(index_parameters,search_parameters)
+    FLANN = cv2.FlannBasedMatcher(index_parameters, search_parameters)
 
     N_neighbors = 2
 
-    matches = FLANN.knnMatch(descriptor_1,descriptor_2,k=N_neighbors)
+    matches = FLANN.knnMatch(descriptor_1, descriptor_2, k=N_neighbors)
 
     match_list = []
 
     threshold = 0.7
 
-    for a,b in matches:
-        if a.distance < threshold*b.distance:
+    for a, b in matches:
+        if a.distance < threshold * b.distance:
             match_list.append(a)
 
     minimum_match = 1000
 
     if len(match_list) > minimum_match:
-        source_points = np.float32([key_points_1[a.queryIdx].pt for a in match_list]).reshape(-1,1,2)
-        destination_points = np.float32([key_points_2[a.trainIdx].pt for a in match_list]).reshape(-1,1,2)
+        source_points = np.float32([key_points_1[a.queryIdx].pt for a in match_list]).reshape(-1, 1, 2)
+        destination_points = np.float32([key_points_2[a.trainIdx].pt for a in match_list]).reshape(-1, 1, 2)
 
         error_threshold = 5
 
-        M, mask = cv2.findHomography(source_points,destination_points,cv2.RANSAC,error_threshold)
+        M, mask = cv2.findHomography(source_points, destination_points, cv2.RANSAC, error_threshold)
 
         match_mask = mask.ravel().tolist()
 
         height, width = image_1.shape
 
-        image_border = np.float64([[0,0],[0,height-1],[width-1,height-1],[width-1,0]]).reshape(-1,1,2)
+        image_border = np.float64([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]]).reshape(-1, 1, 2)
 
-        warped_border = cv2.perspectiveTransform(image_border,M)
+        warped_border = cv2.perspectiveTransform(image_border, M)
 
-        image_2 = cv2.polylines(image_2,[np.int32(warped_border)],True,255,3,cv2.LINE_AA)
+        image_2 = cv2.polylines(image_2, [np.int32(warped_border)], True, 255, 3, cv2.LINE_AA)
 
         print(f"Potential Match Found: {len(match_list)} Matches")
 
@@ -59,7 +63,7 @@ def feature_matching(image_path_1, image_path_2):
         match_mask = None
 
     match_color = (0, 255, 0)
-    point_color = (255,0,255)
+    point_color = (255, 0, 255)
 
     draw_parameters = dict(matchColor=match_color, singlePointColor=point_color, matchesMask=match_mask,
                            flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
@@ -69,6 +73,7 @@ def feature_matching(image_path_1, image_path_2):
     plt.imshow(image_match, "gray")
     plt.show()
     plt.close()
+
 
 if __name__ == "__main__":
     script_path = Path(__file__).resolve()
@@ -88,10 +93,10 @@ if __name__ == "__main__":
 
     print(file_list)
 
-    pair_list = itertools.combinations(file_list,2)
+    pair_list = itertools.combinations(file_list, 2)
 
     for entry in pair_list:
         image_path_1 = entry[0]
         image_path_2 = entry[1]
 
-        feature_matching(image_path_1,image_path_2)
+        feature_matching(image_path_1, image_path_2)
