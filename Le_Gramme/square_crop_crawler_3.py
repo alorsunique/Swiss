76fcff5ae0dcf0cefd4073300fa6c1db07f1124f
alorsunique <sounique7800@gmail.com>
2024-07-25 22:34:14 +0800

    2024 July 25

--- Le_Gramme/square_crop_crawler_3.py
+++ Le_Gramme/square_crop_crawler_3.py
@@ -1,10 +1,6 @@
 import os
 from pathlib import Path
 
-import os
-import time
-from pathlib import Path
-
 from PIL import Image
 
 project_dir = Path.cwd().parent
@@ -13,15 +9,22 @@ os.chdir(project_dir)
 with open("Resources_Path.txt", "r") as resources_text:
     resources_dir = Path(resources_text.readline())
 
-
 rescale_dir = resources_dir / "Rescale"
-era_folder = Path(str(input(f"Path: ")).replace('"', ''))
+source_dir = Path(str(input(f"Path: ")).replace('"', ''))
 
 # Take note of all images in the path provided
 
 image_list = []
-for entry in era_folder.rglob('*'):
-    if entry.is_file():
+for entry in source_dir.rglob('*'):
+
+    try:
+        with Image.open(entry) as opened_image:
+            opened_image.verify()
+            image_check = True
+    except (IOError, SyntaxError):
+        image_check = False
+
+    if entry.is_file() and image_check:
         image_list.append(entry)
 
 
@@ -63,33 +66,11 @@ for image in image_list:
 
     cropped_image = square_crop(working_image)
 
-    horizontal_size = working_image.size[0]
-    vertical_size = working_image.size[1]
+    print(type(cropped_image))
 
-    if horizontal_size < vertical_size:
-        square_ratio = vertical_size / horizontal_size
-    else:
-        square_ratio = horizontal_size / vertical_size
+    if not cropped_image == None:
 
-    if square_ratio > 1.01:
-
-        if horizontal_size < vertical_size:
-            square_size = horizontal_size
-
-            top_pixel = (vertical_size - square_size) // 2
-            bottom_pixel = top_pixel + square_size
-
-            cropped_image = working_image.crop((0, top_pixel, horizontal_size, bottom_pixel))
-
-        else:
-            square_size = vertical_size
-
-            left_pixel = (horizontal_size - square_size) // 2
-            right_pixel = left_pixel + square_size
-
-            cropped_image = working_image.crop((left_pixel, 0, right_pixel, vertical_size))
-
-        relative_path = image.relative_to(era_folder)
+        relative_path = image.relative_to(source_dir)
 
         output_name = image.name
         output_dir = rescale_dir / relative_path.parent / "Cropped"
